936984d260e9831c9efdfeb3ac791897d98c21c5
yashpurkar <yashpurkar7079@gmail.com>
2022-11-27 08:14:11 +0530

    added delete button

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -10,10 +10,22 @@ const ToDo = () => {
       alert("Plzz Fill the data.")
     }
     else {
-      setItems([...items, inputData])
+      const myNewData = {
+        id: new Date().getTime().toString(),
+        name: inputData
+      }
+      setItems([...items, myNewData])
+      setInputData("")
     }
   }
 
+
+  const deleteItem = (id) => {
+    const updatedItems = items.filter((currElem) => {
+      return currElem.id !== id;
+    })
+    setItems(updatedItems);
+  }
 
   return (
     <div className='main_div'>
@@ -36,11 +48,14 @@ const ToDo = () => {
         <div className="showItems">
           <div className="eachItem">
             {
-              items.map((currElem) => {
-                return <><span>{currElem}</span>
-                  <div className="todo_btn" style={{ display: "inline", margin: "35px" }}>
-                    <i className="fa-solid fa-pen-to-square" style={{ margin: "10px" }}></i>
-                    <i className="fa-solid fa-trash"></i>
+              items.map((currElem, index) => {
+                return <>
+                  <div className="eachItem" key={index}>
+                    <span>{currElem.name}</span>
+                    <div className="todo_btn" style={{ display: "inline", margin: "35px" }}>
+                      <i className="fa-solid fa-pen-to-square" style={{ margin: "10px" }}></i>
+                      <i className="fa-solid fa-trash" onClick={() => deleteItem(currElem.id)}></i>
+                    </div>
                   </div>
                   <br />
                 </>
